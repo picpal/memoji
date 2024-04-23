4c6c495f8b0698defbee29634b99b416aa1a1671
picpal <picpal@kakao.com>
2024-04-24 05:14:23 +0900

    pdf download

--- src/MarkdownEditor.jsx
+++ src/MarkdownEditor.jsx
@@ -9,12 +9,46 @@ import { useFetchDocument } from './hooks/useFetchDocument';
 import 'highlight.js/styles/github-dark.css';
 
 
+
 const MarkdownEditor = ({ docId }) => {
   const editorDiv = useRef(null);
   const previewDiv = useRef(null);
   const { docContent, isLoading, error } = useFetchDocument(docId);
   const [preview, setPreview] = useState('');
   const marked = setupMarked();
+
+  const clickHandler = () => {
+    const viewer = previewDiv.current.innerHTML;
+    if(!viewer || viewer === "") {
+      return;
+    }
+
+    downloadPDF(viewer);
+  }
+
+  async function downloadPDF(htmlContent) {
+    const response = await fetch('http://localhost:3000/api/generate-pdf', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ htmlContent })
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch PDF');
+    }
+
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'document.pdf';
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  }
+
 
   useEffect(() => {
     if(!editorDiv.current) return;
@@ -53,9 +87,11 @@ const MarkdownEditor = ({ docId }) => {
 
   return (
     <div className="flex gap-3">
-      <div className="h-screen border border-blue-500 w-44"></div>
+      <div className="h-screen border border-blue-500 w-44">
+        <button onClick={clickHandler}>PDF DOWN</button>
+      </div>
       <div ref={editorDiv} className="h-screen border border-blue-500 flex-1"></div>
-      <div className="viewer h-screen border border-blue-500 flex-1 p-4 overflow-y-auto" dangerouslySetInnerHTML={{ __html: preview }} ref={previewDiv}></div>
+      <div ref={previewDiv} className="viewer h-screen border border-blue-500 flex-1 p-4 overflow-y-auto" dangerouslySetInnerHTML={{ __html: preview }} ></div>
     </div>
   );
 };
